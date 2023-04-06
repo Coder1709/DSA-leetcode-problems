a1caa47c8079f54518eacacca3c9fed0d0fa9078
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-04-06 21:08:09 +0530

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -64,4 +64,36 @@ public:
 
 "have some error because of the dynamic location please provide me a solution if you can"
     
+    "solve this by using an fixed sized vector in place of array "
+    
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> col(m);
+        vector<int> row(n);
+        //int col[m] = {0} ;
+        //int row[n] = {0} ;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(matrix[i][j] == 0 ){
+                    row[i] =1;
+                    col[j] =1;
+                    
+                } 
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if (row[i]  || col[j] ){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        
+    }
+};
+    
   
