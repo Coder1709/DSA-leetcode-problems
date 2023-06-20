458b4103f49a1d36ce5f08aaab5d70f9ac317481
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-06-20 12:28:07 +0530

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+//using map saving occurance count
+
+int main() {
+    
+    int n , i , a;
+    //cin>>n;
+   // string str;
+    stack <int> st;
+    for ( i = 0 ; i < n ; i++){
+        cin>>a;
+        st.push(a);
+    }
+    while( !st.empty()){
+        cout<< st.top() << " ";
+        st.pop();
+        
+    }
+    
+
+    
+    return 0;
+}
